ee8c47a0e59100dc96c330ab1a685b951592e9e1
Vinicio X. López Bonilla <faibolvxlb@gmail.com>
2019-09-12 17:48:54 -0500

    Controlador TipoTecnico

---
+++ src/Source Packages/Controlador/ControladorTipoTecnico.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Clases.TipoTecnico;
+import ModeloDAO.TipoTecnicoDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Xavier
+ */
+public class ControladorTipoTecnico extends HttpServlet {
+
+    String listar = "Vistas/TipoTecnico/listar.jsp";
+    String add = "Vistas/TipoTecnico/add.jsp";
+    String edit = "Vistas/TipoTecnico/edit.jsp";
+    
+    TipoTecnico tipoTecnico=new TipoTecnico();
+    TipoTecnicoDAO tipoTecnicoDAO = new TipoTecnicoDAO();
+    
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ControladorTipoTecnico</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ControladorTipoTecnico at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String acceso="";
+        String action=request.getParameter("accion");
+        if(action.equalsIgnoreCase("listar")){
+            acceso=listar;
+        }
+        RequestDispatcher vista=request.getRequestDispatcher(acceso);
+        vista.forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
